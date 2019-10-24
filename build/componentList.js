7c73e85f16c12ba0b9c6330fec36070e49352f02
zhangchengxin <zhangchengxin@meicai.cn>
2019-10-24 13:47:45 +0800

    feat: add script for building

--- build/componentList.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ReLine: { src: 'src/packages/line/index.js', dist: 'lib/line' },
-  ReBar: { src: 'src/packages/bar/index.js', dist: 'lib/bar' },
-  RePie: { src: 'src/packages/pie/index.js', dist: 'lib/pie' },
-}
--- build/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  { type: 'cjs', min: false, suffix: '.common.js' },
-  { type: 'cjs', min: true, suffix: '.common.min.js' },
-  { type: 'umd', min: false, suffix: '.js' },
-  { type: 'umd', min: true, suffix: '.min.js' }
-]
