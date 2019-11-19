21836e3bfe9824b2d22b65e81eb1cddf7370b2ae
zhangchengxin <zhangchengxin@meicai.cn>
2019-11-19 15:54:05 +0800

    update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@
 
 module.exports = {
 	root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  env: {
-    es6: true,
-    node: true,
-    browser: true
+	parserOptions: {
+		parser: 'babel-eslint',
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	env: {
+		es6: true,
+		node: true,
+		browser: true
 	},
 	extends: [
 		'standard',
 		'plugin:react/recommended'
-  ],
+	],
 	rules: {
 		'semi': ['error', 'always'],
 		'space-before-function-paren': ['error', 'never'],
