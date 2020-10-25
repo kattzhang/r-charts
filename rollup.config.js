4519fb69ca5c5aceb3958293cef2f0d2d5ffb0a7
kimozhang <1193055507@qq.com>
2020-10-25 14:47:25 +0800

    refactor: production mode

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+const cjs = require('rollup-plugin-commonjs')
+
+module.exports = {
+  input: './src/index.js',
+  output:[
+    {
+      format: 'esm',
+      file: 'dist/r-echarts.esm.js'
+    },
+    {
+      format: 'cjs',
+      file: 'dist/r-echarts.common.js',
+      exports: 'auto'
+    },
+  ],
+  plugins: [
+    resolve(),
+    babel({ exclude: ['node_modules/**'] }),
+    cjs()
+  ],
+  external: id => /^(react|prop-types|lodash|echarts)/.test(id),
+}
